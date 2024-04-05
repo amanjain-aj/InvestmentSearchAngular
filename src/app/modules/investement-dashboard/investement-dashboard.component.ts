d13cef41e492f89d283b9c5af25ab4fdd092c32b
supriyamahalik937 <80851191+supriyamahalik937@users.noreply.github.com>
2024-04-05 14:58:20 +0530

    ui enhancement

--- src/app/modules/investement-dashboard/investement-dashboard.component.ts
+++ src/app/modules/investement-dashboard/investement-dashboard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-investement-dashboard',
   templateUrl: './investement-dashboard.component.html',
-  styleUrls: ['./investement-dashboard.component.scss']
+  styleUrls: ['./investement-dashboard.component.scss'],
 })
 export class InvestementDashboardComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    // Use the back method of the Location service to go back to the previous page
+    this.location.back();
+  }
 }
